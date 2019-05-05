f3b2e8b4de66b2431a0b2e1093b4b9dcc4f16402
shlee0882 <shlee0882@gmail.com>
2019-05-05 18:09:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Wetube
+
+Cloning Youtube with JS and NodeJS
---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hi!");
+
+/*
+
+1. node js 설치
+2. 자신의 작업폴더 생성 후 이동
+3. npm init
+4. npm install express
+5. 협업 시 package-lock.json, node_modules는 올리지 않아도 된다.
+6. package.json파일 하나만 있으면 node install 명령을 통해 
+node_modules, package-lock.json 파일을 받기 때문이다. 
+
+*/
+
