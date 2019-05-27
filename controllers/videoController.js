d9a2433d94526070c129380e6073eb30fddf6ab1
shlee0882 <shlee0882@gmail.com>
2019-05-27 22:08:27 +0900

    add eslint and search function

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,19 +3,27 @@ import Video from  "../models/Video";
 
 export const home = async(req, res) =>{
     try{
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({_id: -1});
         res.render("home", { pageTitle: 'Home', videos});
     } catch(error){
         console.log(error);
         res.render("home", { pageTitle: 'Home', videos: []});
     }
 } 
-export const search = (req, res) => {
+export const search = async(req, res) => {
     console.log(req.query);
     // {query: term}은 (req.query.term)과 같다. 
     const {
         query: {term : searchingBy }
     } = req;
+    let videos = [];
+    try{
+        videos = await Video.find({
+            title: {$regex: searchingBy, $options: "i" }
+        });
+    }catch(error){
+        console.log(error);
+    }
     res.render("search", { pageTitle: 'Search', searchingBy, videos });
 }
 export const getUpload = (req, res) =>
@@ -83,6 +91,8 @@ export const deleteVideo = async (req, res) => {
     } = req;
     try{
         await Video.findOneAndRemove({_id: id});
-    }catch(error){}
+    }catch(error){
+        console.log("error");
+    }
     res.redirect(routes.home);
 }
--- myNote/mongoDB1.md
+++ myNote/mongoDB1.md
@@ -30,3 +30,42 @@ use we-tube
 show collections
 db.videos.remove({})
 ```
+
+### eslinst 설치하기
+
+```bash
+
+1. remove if you installed eslint globally  $ npm uninstall eslint -g
+
+2. install  eslint extension on vscode
+https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+
+3.  install 3 packages manually, not by command on eslint installed globally, 
+$ yarn add eslint-config-airbnb-base eslint eslint-plugin-import
+
+4. make .eslintrc.js file and copy paste codes below
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {}
+};
+
+5. install eslint-config-prettier $yarn add eslint-config-prettier
+
+6. go to .eslintrc.js file and change "extends" from   "airbnb-base", to ["airbnb-base", "prettier"],
+
+7. go to app.js and try save by ctrl+s  then you will see red lines if eslint activates succesfully
+
+```
